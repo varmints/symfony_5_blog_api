6341cb46417596fef7ce87b97e990a9ff87e366f
Kamil <you@example.com>
2021-06-22 20:11:36 +0200

    add basic security login functionality

---
+++ blog_api/src/Controller/SecurityController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login", methods={"POST"})
+     */
+    public function login() {
+        if(!$this->isGranted('IS_AUTHENTICATED_FULL')) {
+            return $this->json([
+                'error' => 'Invalid login request: check that the Content-Type header is "application/json"'
+            ], 400);
+        }
+
+        return $this->json([
+            'user' => $this->getUser() ? $this->getUser()->getId() : null,
+        ]);
+    }
+}
